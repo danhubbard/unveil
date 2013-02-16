a158c47c643f73b1e035f57dd861b2dd7c7bea67
Joe James <joe.james@alliants.co.uk>
2013-02-16 21:39:53 +0000

    add hack for favicon error
    Some browsers automatically request the favicon, but we're not
    handling static files yet, so we leave a horrible hack to return
    a 404 when the favicon is requested.

--- lib/unveil.rb
+++ lib/unveil.rb
@@ -5,6 +5,8 @@ require "unveil/humanize"
 module Unveil
   class Application
     def call(env)
+      return four_oh_four if unsupported_file_request(env)
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.public_send(act)
@@ -15,6 +17,19 @@ module Unveil
         [text]
       ]
     end
+
+    private
+
+    # HACK
+    def unsupported_file_request(env)
+      env["PATH_INFO"] == '/favicon.ico'
+    end
+
+    def four_oh_four
+      [
+        404,
+        { 'Content-Type' => 'text/html'},
+        []
       ]
     end
   end
