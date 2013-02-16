c151085fca04d728a8a6ae08e49d89344dd46c99
Joe James <joe.james@alliants.co.uk>
2013-02-16 21:39:26 +0000

    add basic routing to controller actions

--- lib/unveil.rb
+++ lib/unveil.rb
@@ -1,15 +1,28 @@
 require "unveil/version"
+require "unveil/routing"
 require "unveil/humanize"
 
 module Unveil
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.public_send(act)
+
       [
         200,
         { 'Content-Type' => 'text/html' },
-        ['Hello to an unveiled world!']
+        [text]
       ]
+    end
+      ]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+    def initialize env
+      @env = env
     end
   end
 end
